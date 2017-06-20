5b432e5806b7cf776e5bb436d253e2ad5b8ae3f6
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-06-20 16:03:13 +0300

    Initial commit

---
+++ src/GameHelper.java
@@ -0,0 +1,21 @@
+/**
+ * Created by esa on 20.06.2017.
+ */
+import java.io.*;
+
+public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.print(prompt + "  ");
+        try {
+            BufferedReader is = new BufferedReader(
+                    new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0 )  return null;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        //return inputLine.toLowerCase();
+        return inputLine;
+    }
+}
---
+++ src/SimpleDotComTestDrive.java
@@ -0,0 +1,23 @@
+/**
+ * Created by esa on 20.06.2017.
+ */
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("Потопил")) {
+                isAlive = false;
+                System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
+            }
+        }
+    }
+}
